3098a64099114ef72b5b6b1247b6c4ec7042b238
Cindy Wong <cwong@gmail.com>
2022-08-29 23:54:51 -0700

    adding initial script file

---
+++ script.py
@@ -0,0 +1,14 @@
+"""
+A program that stores book information:
+- Title, Author
+- Year, ISBN
+
+User can:
+- View all records
+- Search an entry
+- Add an entry
+- Update entry
+- Delete
+- Close
+
+"""
