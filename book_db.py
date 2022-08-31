7359079da3d49eb447a2d48fd6cb8a544a7d5741
Cindy Wong <cwong690@gmail.com>
2022-08-31 00:12:17 -0700

    add postgresql creds file

---
+++ book_db.py
@@ -0,0 +1,47 @@
+import psycopg2
+
+def create_table():
+    # Create connection to sqlite3. If no database, new one will be created
+    conn = sqlite3.connect("lite.db")
+
+    # Add cursor
+    cur = conn.cursor()
+
+    # Create table and add value
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+
+    # Commit changes and close
+    conn.commit()
+    conn.close()
+
+def insert_in(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+def view_db():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?", (item,))
+    conn.commit()
+    conn.close()
+
+def update(quantity, price, item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+update(11, 9, "Wine Glass")
+print(view_db())
