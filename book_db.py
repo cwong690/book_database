24546f8cb5d54dd179edbc8246ca265488db5faa
Cindy Wong <cwong690@gmail.com>
2022-09-01 00:30:29 -0700

    create title boxes with listbox

---
+++ book_db.py
@@ -0,0 +1,47 @@
+"""
+Create program that stores book information such as Title, Author, Year and ISBN
+Allows users to view all records, search/add/update entry, delete and close
+"""
+
+from tkinter import *
+
+window=Tk()
+
+# Create labels for entry boxes
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
+l2 = Label(window, text="Author")
+l2.grid(row=0, column=2)
+l3 = Label(window, text="Year")
+l3.grid(row=1, column=0)
+l4 = Label(window, text="ISBN")
+l4.grid(row=1, column=2)
+
+# Create entry boxes
+title_text=StringVar()
+e1 = Entry(window, textvariable=title_text)
+e1.grid(row=0, column=1)
+
+author_text=StringVar()
+e2 = Entry(window, textvariable=author_text)
+e2.grid(row=0, column=3)
+
+year_text=StringVar()
+e3 = Entry(window, textvariable=year_text)
+e3.grid(row=1, column=1)
+
+isbn_text=StringVar()
+e4 = Entry(window, textvariable=isbn_text)
+e4.grid(row=1, column=3)
+
+# Create list box
+list1 = Listbox(window, height=6, width=35)
+list1.grid(row=2, column=0, rowspan=6, columnspan=2)
+
+# Create scrollbar then configure to listbox
+sb1 = Scrollbar(window)
+sb1.grid(row=2, column=2)
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+window.mainloop()
